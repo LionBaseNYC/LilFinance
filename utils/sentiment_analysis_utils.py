56153610a2bfc9475b131e40169dfc5f8a89601c
Ongun Uzay Macar <oum2000@columbia.edu>
2019-04-13 02:57:52 -0400

    Added more utilities and provided a README for their usage.

--- utils/sentiment_analysis_utils.py
+++ utils/sentiment_analysis_utils.py
@@ -2,16 +2,128 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon March 4 02:19:56 2019
-Utilities included for performing sentiment analysis, adapted from
-LionBase's Sentiment Analysis workshop.
+Utilities included for use by Naive Bayes and RNN w/ LSTM model training and
+data preparation, and also performing static sentiment analysis which is
+roughly adapted from LionBase's Sentiment Analysis workshop.
 """
-
+# Standard imports
+import numpy as np
+# Imports for getting online sentiment data
 import requests
+# Imports for punctuating removing
+from string import punctuation
+# Imports for tokenization
+from collections import Counter
 
+# UTILITIES USED BY RNN w/ LSTM MODEL AND ITS TRAINING #
+def remove_punctuation_and_lower(str_arr):
+    """
+    Function to convert the strings inside an array into lowercase format
+    and remove punctuation.
+    @param str_arr: array of texts, be it reviews or sentences
+    @return clean_str_arr: cleaned string array
+    """
+    clean_str_arr = [None] * len(str_arr)
+    for i in range(len(str_arr)):
+        # Convert  text into lowercase format
+        clean_str_arr[i] = str_arr[i].lower()
+        # Remove punctuation from text
+        clean_str_arr[i] = ''.join([c for c in clean_str_arr[i] if c not in punctuation])
+    return clean_str_arr
+
+def get_tokenization_map(str_arr, K = 1):
+    """
+    Function to create a vocabulary-to-integer index mapping dictionary,
+    where frequently occurring words are assigned to lower indexes.
+    @param str_arr: array of texts, be it reviews or sentences
+    @param K: most common words factor, decides how many words
+         the mapping dictionary contains. Set to 1 for tokenization.
+    @return vocabulary_to_integer: mapping dictionary
+    """
+    all_words = []
+    for i in range(len(str_arr)):
+        # Break down into list of words
+        for word in str_arr[i].split():
+            all_words.append(word)
+
+    # Count all the words using Counter Method
+    words_with_counts = Counter(all_words)
+    count_sorted_words = words_with_counts.most_common(int(len(all_words)*K)) # get all words
+    # print(str(count_sorted_words)) # debugging
+    # Create mapping dictionary
+    vocabulary_to_integer = {word:index+1 for index, (word,count) in enumerate(count_sorted_words)}
+    # print(vocabulary_to_integer) # debugging
+    return vocabulary_to_integer
+
+def get_encoding(str_arr, vocabulary_to_integer, predicting=False):
+    """
+    Function to encode each word in each string in the array into its corresponding
+    integer index defined in the passed tokenization mapping dictionary.
+    @param str_arr: array of texts, be it reviews or sentences
+    @param vocabulary_to_integer: tokenization mapping dictionary
+    @param predicting: boolean value to alert us to check if text (array) is newly seen
+    @return encoded_arr: array of encoded integer arrays
+    """
+    encoded_arr = []
+    for text in str_arr:
+        # If text is newly seen, check if mapping includes the passed words
+        mapped = []
+        if predicting:
+            sign = lambda x: math.copysign(1, x) if x!=0 else 0
+            corresponding = lambda x: vocabulary_to_integer[x] if x in vocabulary_to_integer.keys() else 0
+            mapped = [corresponding(word)  for word in text.split()]
+        else:
+            mapped = [vocabulary_to_integer[word] for word in text.split()]
+        encoded_arr.append(mapped)
+    return encoded_arr
+
+def pad_or_truncate(encoded_arr, sequence_length):
+    """
+    Function to pad the encoded array with 0s or truncate it,
+    depending on its length with comparison to specified sequence length.
+    @param encoded_arr: Encoded (using vocab-to-int map) text array
+    @param sequence_length (int): normalizing length for reviews,
+           also known as the number of steps for the LSTM layer,
+           and also the number of features for each input in the model.
+    @return features: padded OR truncated encoded array, will serve
+            as a input for the model.
+    """
+    features = np.zeros((len(encoded_arr), sequence_length), dtype=int)
+
+    for i, encoded_text in enumerate(encoded_arr):
+        text_length = len(encoded_text)
+        if text_length <= sequence_length:
+            normalized_text = encoded_text + list(np.zeros(sequence_length - text_length, dtype=int))
+        elif text_length > sequence_length:
+            normalized_text = encoded_text[0:sequence_length]
+
+        features[i,:] = np.array([int(id) for id in normalized_text])
+
+    return list(features)
+
+def sample_predict(model, X, vocabulary_to_integer, sequence_length):
+    """
+    Function to send a sample input and retrieve predictions from a
+    Keras model only.
+    @param model: Compiled Keras model class
+    @param X: Input
+    @param vocabulary_to_integer: Mapping
+    @sequence_length: Sequence length
+    @return predictions
+    """
+    clean_X = remove_punctuation_and_lower([X])
+    encoded = get_encoding(clean_X, vocabulary_to_integer, predicting=True)
+    equalized = pad_or_truncate(encoded, sequence_length)
+    return model.predict(x=np.array(equalized))
+    # NOTE: If equalized was a singular string, we could have
+    # surrounded it with [ ] (square brackets) as np.array
+    # asks for a list to convert to NumPy format
+
+# UTILITIES USED BY STATIC SENTIMENT ANALYSIS METHODS #
 def listify(text):
     """
     Function to convert a string text into a list of words.
-    @arg text: space seperated words
+    @param text: space seperated words
     @return words: list of lowercase words
     """
     words = text.lower().split(' ')
@@ -34,7 +146,11 @@ def listify(text):
 def get_positive_negative_words():
     """
     Function to get positive and negative words based on Hu and Liu's
-    sentiment analysis.
+    sentiment analysis. Please refer to their following paper:
+    Minqing Hu and Bing Liu. "Mining and Summarizing Customer Reviews." Proceedings of the ACM
+    SIGKDD International Conference on Knowledge Discovery and Data Mining (KDD-2004), Aug 22-25,
+    2004, Seattle, Washington, USA.
+
     @return (positive_words, negative_words): tuple of two lists, representing +/- words
     """
     lists = []
